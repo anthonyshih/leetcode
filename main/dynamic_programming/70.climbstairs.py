9d96d886f5b40ba22dd9ce50152e3962bfa37da0
anthonyshih <143860292+anthonyshih@users.noreply.github.com>
2023-09-05 10:36:38 +0800

    Update 70.climbstairs.py

--- main/dynamic_programming/70.climbstairs.py
+++ main/dynamic_programming/70.climbstairs.py
@@ -73,12 +73,22 @@ Fn = Fn-1 + Fn-2
             
 
 '''
-def climbstarirs(n):
-    dp = [0]*(n+1)
-    dp[1] = 1
-    dp[2] = 2
-    for i in range(3,n+1):
-        dp[i] = dp[i-2] + dp[i-1]
-    return dp[-1]
+
+def climbStairs(self, n):
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        
+        dp = [0] * (n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 2] + dp[i - 1]
+        
+        return dp[-1]
+    
+
 
 print(climbstarirs(5))
