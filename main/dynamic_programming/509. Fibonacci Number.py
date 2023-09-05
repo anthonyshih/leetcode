bd4981e8b7262eb00ed12bcb8be1a36692950baf
anthonyshih <143860292+anthonyshih@users.noreply.github.com>
2023-09-05 10:31:32 +0800

    Update 509. Fibonacci Number.py

--- main/dynamic_programming/509. Fibonacci Number.py
+++ main/dynamic_programming/509. Fibonacci Number.py
@@ -24,12 +24,15 @@ Output: 3
 Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
 
 '''
-def Fibnum(n):
-    fib =[0]*(n+1)
-    fib[0] = 0
-    fib[1] = 1
-    for i in range(2,n+1):
-        fib[i] = fib[i-1] + fib[i-2]
-    return fib[-1]
+def fib(self, n):
+        fib = [0] * (n + 1)
+        fib[0] = 0
+        if n > 0:
+            fib[1] = 1
+        for i in range(2, n + 1):
+            fib[i] = fib[i - 1] + fib[i - 2]
+        return fib[-1]
+    
 
-print(Fibnum(4))
+
+print(fib(4))
